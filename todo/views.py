c99f607c997d7ef54368ba0aca3dded3e85b5fd9
omkart <omkartupere7176@gmail.com>
2024-03-01 20:03:24 +0530

    Adding complete and delete operation

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,8 @@ from .models import Todo
 
 # Create your views here.
 def todo_list(request):
-    return render(request, 'todo/index.html')
+    todos = Todo.objects.all()
+    return render(request, 'todo/index.html', {'todos':todos})
 
 def create_todo(request):
     if request.method == 'POST':
@@ -11,4 +12,16 @@ def create_todo(request):
         description = request.POST.get('description')
         Todo.objects.create(title=title, description=description)
     
+    return redirect('todo_list')
+
+def complete_todo(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.completed = True
+    todo.save()
+    return redirect('todo_list')
+
+def delete_todo(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+
     return redirect('todo_list')
